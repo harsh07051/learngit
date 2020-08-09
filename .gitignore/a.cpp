4d83bc8211b5b6dff271f858f6cb292ad0a65d92
HarshitKumarSinghal <harshit07051998@gmail.com>
2020-08-09 18:28:33 +0530

    hi

--- .gitignore/a.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout<<"This is my secret code";
-	cout<<"Stay the  out of this file";
-	return 0;
-}
---
+++ .gitignore/pen.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"This is a pandemic situation";
+	return 0;
+}
